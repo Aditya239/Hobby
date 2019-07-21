f114500d3db66cf6800bf863f9ba247168ae36d4
Blastoise <ashutoshkumarranchi1@gmail.com>
2019-07-21 20:07:26 +0530

    percolation problem added

---
+++ Percolation Thresholds in Square/percolation.py
@@ -0,0 +1,84 @@
+from __future__ import division
+import random
+import matplotlib.pyplot as plt
+import sys
+
+def generate(N,p):
+    M = []
+    list_row = []
+    for i in range(N):
+        for j in range(N):
+            l = random.random()
+            if(l<=p):
+                list_row+=[1]
+            else:
+                list_row+=[0]
+        M+=[list_row]		
+    return M
+
+        	
+def percolate(M):
+    for i in range(N):
+	if(M[0][i]==1):
+            M[0][i]==2
+    
+    for i in range(N-1):
+    	for j in range(N):
+    	    if(M[i+1][j]==1):
+    	        if(M[i][j]==1 or M[i][j]==2):
+    		    M[i+1][j]==2
+    
+    for i in range(N-1):
+        for j in range(N):
+            if(M[i+1][j]==1):
+                if(j+1<=N and M[i+1][j+1]==2):
+                    M[i+1][j]==2
+                elif(j-1>=0 and M[i+1][j-1]==2):
+                    M[i+1][j]==2
+    for i in range(N):
+        if(M[N-1][i]==2):
+            return 1
+    return 0
+    
+def plot(x,y):
+    plt.scatter(x, y, label= "stars", color= "green", marker= "*", s= 30)
+    plt.xlabel('Values of p')
+    plt.ylabel('Probability of percolation')
+    plt.legend()
+    plt.show()   
+    
+def simulate(T,N,p):
+    u=[]
+    probability=0
+    for i in range(T):
+    	u=generate(N,p)
+    	probability+=percolate(u)
+    return probability/T
+
+def main(T,N):
+    y=[]
+    x=[]
+    for i in range(121):
+        p=0.2+i*0.005
+	x+=[p]
+	y+=[simulate(T,N,p)]
+    plot(x,y)
+
+	
+if __name__ == '__main__':
+    if len(sys.argv)!=3:
+        print("Wrong number of arguments") 
+        sys.exit()
+
+    try:
+        T = int(sys.argv[1])
+ 	N = int(sys.argv[2])
+ 	
+    except:
+        print 'Expected input type integer, obtained: ', sys.argv[1], sys.argv[2]
+        sys.exit()
+
+    main(T,N)
+        
+
+
